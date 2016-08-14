783d63a7ba7da8e2ee8e8957cf3a071adf2550d7
Sangwon Lee <gamzabaw@gmail.com>
2016-08-14 22:02:13 +0900

    open stage info modal

--- frontend/src/components/dash/dash.component.ts
+++ frontend/src/components/dash/dash.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { StageService } from '../../services/stage.service';
 import { NavBarComponent } from '../nav/nav-bar.component';
 
 import { StageCardComponent } from './card/stage-card.component';
+import { StageInfoComponent } from './info/stage-info.component';
 import { Stage } from '../../models/Stage';
 
 @Component({
@@ -11,14 +12,19 @@ import { Stage } from '../../models/Stage';
     templateUrl: 'dash.component.html',
     styleUrls: ['dash.component.css'],
     providers: [StageService],
-    directives: [NavBarComponent, StageCardComponent]
+    directives: [NavBarComponent, StageCardComponent, StageInfoComponent]
 })
 export class DashComponent implements OnInit {
+    @ViewChild('stageInfoModal') stageInfoModal: StageInfoComponent;
     stages: Stage[];
 
     constructor(private stageService: StageService) {}
 
     ngOnInit() {
         this.stageService.getStages().then(stages => this.stages = stages);
+    }
+
+    onStageInfoClicked(event: any) {
+        this.stageInfoModal.showModal(event.value);
     }
 }
