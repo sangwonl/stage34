dd5ace8b6a0b78c384af50d296897907ce15330c
Sangwon Lee <gamzabaw@gmail.com>
2016-08-23 23:32:33 +0900

    rest api - create stage

--- webapp/api/handlers/stage.py
+++ webapp/api/handlers/stage.py
@@ -23,3 +23,20 @@ class StageRootHandler(AuthRequiredMixin, View):
         stages_qs = Stage.objects.filter(org=org)
         stages = [model_to_dict(s, fields=RES_FIELDS) for s in stages_qs]
         return JSENDSuccess(status_code=200, data=stages)
+
+    def post(self, request, *args, **kwargs):
+        json_body = json.loads(request.body)
+        title = json_body.get('title')
+        repo = json_body.get('repo')
+        branch = json_body.get('branch')
+
+        if not (title and repo and branch):
+            return JSENDError(status_code=400, msg='invalid stage info')
+
+        org = Membership.get_org_of_user(request.user)
+        if not org:
+            return JSENDError(status_code=400, msg='org not found')
+
+        stage = Stage.objects.create(org=org, title=title, repo=repo, branch=branch)
+        stage_dict = model_to_dict(stage, fields=RES_FIELDS)
+        return JSENDSuccess(status_code=200, data=stage_dict)
