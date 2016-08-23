1a50be59c130ec0a5ec0b45312e89d34d33bf618
Sangwon Lee <gamzabaw@gmail.com>
2016-08-24 00:37:19 +0900

    extract getting stage by id

--- webapp/api/handlers/stage.py
+++ webapp/api/handlers/stage.py
@@ -43,58 +43,55 @@ class StageRootHandler(AuthRequiredMixin, View):
 
 
 class StageDetailHandler(AuthRequiredMixin, View):
+    def get_stage(self, org, stage_id):
+        try:
+            stage = Stage.objects.get(org=org, id=stage_id)
+        except Stage.DoesNotExist:
+            return None
+        return stage
+
     def get(self, request, stage_id, *args, **kwargs):
         org = Membership.get_org_of_user(request.user)
         if not org:
             return JSENDError(status_code=400, msg='org not found')
 
-        try:
-            stage = Stage.objects.get(org=org, id=stage_id)
-        except Stage.DoesNotExist:
+        stage = self.get_stage(org, stage_id)
+        if not stage:
             return JSENDError(status_code=404, msg='stage not found')
 
         stage_dict = model_to_dict(stage, fields=RES_FIELDS)
         return JSENDSuccess(status_code=200, data=stage_dict)
 
     def put(self, request, stage_id, *args, **kwargs):
-        json_body = json.loads(request.body)
-        title = json_body.get('title')
-        repo = json_body.get('repo')
-        branch = json_body.get('branch')
-        status = json_body.get('status')
-        commits = json_body.get('commits')
-        endpoint = json_body.get('endpoint')
-
         org = Membership.get_org_of_user(request.user)
         if not org:
             return JSENDError(status_code=400, msg='org not found')
 
-        try:
-            stage = Stage.objects.get(org=org, id=stage_id)
-        except Stage.DoesNotExist:
+        stage = self.get_stage(org, stage_id)
+        if not stage:
             return JSENDError(status_code=404, msg='stage not found')
 
-        stage.title = title if title else stage.title
-        stage.repo = repo if repo else stage.repo
-        stage.branch = branch if branch else stage.branch
-        stage.status = status if status else stage.status
-        stage.commits = commits if commits else stage.commits
-        stage.endpoint = endpoint if endpoint else stage.endpoint
+        json_body = json.loads(request.body)
+        stage.title = json_body.get('title', stage.title)
+        stage.repo = json_body.get('repo', stage.repo)
+        stage.branch = json_body.get('branch', stage.branch)
+        stage.status = json_body.get('status', stage.status)
+        stage.commits = json_body.get('commits', stage.commits)
+        stage.endpoint = json_body.get('endpoint', stage.endpoint)
         stage.save()
 
         stage_dict = model_to_dict(stage, fields=RES_FIELDS)
         return JSENDSuccess(status_code=204, data=stage_dict)
-
 
     def delete(self, request, stage_id, *args, **kwargs):
         org = Membership.get_org_of_user(request.user)
         if not org:
             return JSENDError(status_code=400, msg='org not found')
 
-        try:
-            stage = Stage.objects.get(org=org, id=stage_id)
-        except Stage.DoesNotExist:
+        stage = self.get_stage(org, stage_id)
+        if not stage:
             return JSENDError(status_code=404, msg='stage not found')
 
         stage.delete()
+
         return JSENDSuccess(status_code=204, data={})
