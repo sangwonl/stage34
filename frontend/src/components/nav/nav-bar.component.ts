9239d91c7d0f5a3f9b041f2530e9bc4f9e9f4c09
Sangwon Lee <gamzabaw@gmail.com>
2016-08-25 23:09:33 +0900

    handle logout

--- frontend/src/components/nav/nav-bar.component.ts
+++ frontend/src/components/nav/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
     selector: 'nav-bar',
     templateUrl: 'nav-bar.component.html',
@@ -8,7 +10,15 @@ import { Router, ROUTER_DIRECTIVES } from '@angular/router';
     directives: [ROUTER_DIRECTIVES]
 })
 export class NavBarComponent implements OnInit {
-    constructor(private router: Router) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
 
     ngOnInit() {}
+
+    private onLogout() {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+    }
 }
