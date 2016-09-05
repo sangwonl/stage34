40b34ee97f457f50cd02e7f1d3c07df8d4199b90
Sangwon Lee <gamzabaw@gmail.com>
2016-09-05 10:57:06 +0900

    extract stage update function

--- webapp/worker/tasks/deployment.py
+++ webapp/worker/tasks/deployment.py
@@ -7,57 +7,59 @@ from api.models.resources import Stage
 from libs.backends.provision import DockerComposeLocal
 
 
+def _udpate_stage_status(stage_id, status):
+    Stage.objects.filter(id=stage_id).update(status=status)
+
+
+def _get_stage_by_id(stage_id):
+    return Stage.objects.filter(id=stage_id).first()
+
+
 @shared_task(queue='q_default')
 def task_provision_stage(github_access_key, stage_id, repo, branch, run_on_create):
-    def _udpate_stage_status(status):
-        Stage.objects.filter(id=stage_id).update(status=status)
-
     # get proper provision backend
     provision_backend = DockerComposeLocal(stage_id, repo, branch, github_access_key)
 
     # clone the repository on the directory
     result = provision_backend.clone_repository()
     if not result:
-        _udpate_stage_status('paused')
+        _udpate_stage_status(stage_id, 'paused')
         return 'error'
 
     # load docker compose file
     result = provision_backend.load_compose_file()
     if not result:
-        _udpate_stage_status('paused')
+        _udpate_stage_status(stage_id, 'paused')
         return 'error'
 
     # write docker provision conf
     result = provision_backend.prepare_provision_conf()
     if not result:
-        _udpate_stage_status('paused')
+        _udpate_stage_status(stage_id, 'paused')
         return 'error'
 
     # skip running containers updating stage status to paused if not run_on_create 
     if not run_on_create:
-        _udpate_stage_status('paused')
+        _udpate_stage_status(stage_id, 'paused')
         return 'ok'
 
     # docker compose up
     result = provision_backend.up()
     if not result:
-        _udpate_stage_status('paused')
+        _udpate_stage_status(stage_id, 'paused')
         return 'error'
 
     # update stage status and connect info
-    _udpate_stage_status('running')
+    _udpate_stage_status(stage_id, 'running')
     return 'ok'
 
 
 @shared_task(queue='q_default')
 def task_change_stage_status(github_access_key, stage_id, action):
-    def _udpate_stage_status(status):
-        Stage.objects.filter(id=stage_id).update(status=status)
-
     if action not in ('start', 'stop'):
         return 'error'
 
-    stage = Stage.objects.filter(id=stage_id).first()
+    stage = _get_stage_by_id(stage_id)
     if not stage:
         return 'error'
 
@@ -78,5 +80,5 @@ def task_change_stage_status(github_access_key, stage_id, action):
         return 'error'
 
     # update stage status
-    _udpate_stage_status(new_status)
+    _udpate_stage_status(stage_id, new_status)
  
