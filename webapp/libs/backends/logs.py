e4d927b4f011ec59891c75075ab25e8f17b6a185
Sangwon Lee <gamzabaw@gmail.com>
2016-09-28 00:18:56 +0900

    provision logging to log file

---
+++ webapp/libs/backends/logs.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+
+import os
+
+
+class LogBucket(object):
+    def __init__(self, name):
+        self.log_path = os.path.join(settings.STORAGE_HOME, name, 'output.log')
+        self.logs = []
+    
+    def put(self, log_message, header=False):
+        if header:
+            log_message = '\n' + log_message
+        self.logs.append(log_message)
+
+    def flush(self):
+        full_log = '\n'.join(self.logs)
+        try:
+            f = open(self.log_path, 'wt')
+            f.write(full_log)
+        except IOError:
+            pass
